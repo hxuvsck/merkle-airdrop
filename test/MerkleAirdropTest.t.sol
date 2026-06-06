84b6e6d6859690255072593b8beb2a77ca2828fb
hxuvsck <hxuvsck@gmail.com>
2026-06-06 20:06:43 +0800

    Test setup add

--- test/MerkleAirdropTest.t.sol
+++ test/MerkleAirdropTest.t.sol
@@ -1,8 +1,26 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
 
-import {Test} from "forge-std/Test.sol";
+import {Test, console} from "forge-std/Test.sol";
+import {MerkleAirdrop} from "../src/MerkleAirdrop.sol";
+import {BagelToken} from "../src/BagelToken.sol";
 
 contract MerkleAirdropTest is Test {
-    function setUp() external {}
+    MerkleAirdrop public airdrop;
+    BagelToken public token;
+
+    bytes32 public ROOT = 0xaa5d581231e596618465a56aa0f5870ba6e20785fe436d5bfb82b08662ccc7c4;
+    address user;
+    uint256 userPrivateKey;
+
+    function setUp() public {
+        token = new BagelToken();
+        airdrop = new MerkleAirdrop(ROOT, token);
+        // by adding the whitelist index 0 a generated address, we now can test it as it has already added in airdrop list
+        (user, userPrivateKey) = makeAddrAndKey("user");
+    }
+
+    function testUsersCanClaim() public {
+        // console.log("user address:", user); // this will be firstly used to generate the user address and adding them to scripts of generating the merkle tree input and outputs to addin the function whitelist array index 0 to test the contract
+    }
 }
